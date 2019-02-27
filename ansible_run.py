ba7d742fde3f4a0a7287e0284146d90a80ce84cc
Jeff Hibberd <jeff@jeffhibberd.com>
2019-02-27 14:15:15 +0000

    Revert "remove locking code now that we have a queue"
    This reverts commit f712789e1ec8ceac06ed3b47c7a76c2f7b8fbe2f.

--- ansible_run.py
+++ ansible_run.py
@@ -1,13 +1,15 @@
 """Functions to run ansible playbooks."""
 import os
 import subprocess
-from multiprocessing import Pool
+from multiprocessing import Pool, Lock
 
 import anmad_args
 import anmad_logging
 
+PLAYBOOK_LOCK = Lock()
+
 def run_one_playbook(my_playbook):
-    """Run exactly one ansible playbook. Don't call this
+    """Run exactly one ansible playbook. Due to locking, don't call this
     directly. Instead, call one of the multi-playbook funcs with a list of
     one playbook eg [playbook]."""
 
@@ -37,18 +39,31 @@ def run_one_playbook(my_playbook):
 def runplaybooks(listofplaybooks):
     """Run a list of ansible playbooks and wait for them to finish."""
 
+    anmad_logging.LOGGER.debug("%s Waiting for playbook lock", __name__)
+    PLAYBOOK_LOCK.acquire()
+    anmad_logging.LOGGER.debug("Lock acquired")
+
     pool = Pool(anmad_args.ARGS.concurrency)
     ret = pool.map(run_one_playbook, listofplaybooks)
     pool.close()
     pool.join()
 
+    PLAYBOOK_LOCK.release()
+    anmad_logging.LOGGER.debug("Lock released")
     return ret
 
 def runplaybooks_async(listofplaybooks):
     """Run a list of ansible playbooks asyncronously."""
+
+    anmad_logging.LOGGER.debug("%s Waiting for playbook lock", __name__)
+    PLAYBOOK_LOCK.acquire()
+    anmad_logging.LOGGER.debug("Lock acquired")
+
     pool = Pool(anmad_args.ARGS.concurrency)
     ret = pool.map_async(run_one_playbook, listofplaybooks)
     pool.close()
     pool.join()
 
+    PLAYBOOK_LOCK.release()
+    anmad_logging.LOGGER.debug("Lock released")
     return ret

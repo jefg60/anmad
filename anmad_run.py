cdecb3b4d025490c535c18c10d6f00bab78a867e
Jeff Hibberd <jeff@jeffhibberd.com>
2019-02-27 15:41:18 +0000

    anmad_run with pre-run queue

--- anmad_run.py
+++ anmad_run.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
-"""Daemon to watch a redis queue for ansible jobs."""
+"""Daemon to watch redis queues for ansible jobs."""
 
 from hotqueue import HotQueue
-queue = HotQueue('playbooks')
+PLAYQ = HotQueue('playbooks')
+PREQ = HotQueue('prerun')
 
-for item in queue.consume():
-    if type(item) is not list:
-        print('WARNING: %s from playbook queue is not a list, ignoring it', item)
-    else:
-        print(item)
+for playbookjob in PLAYQ.consume():
+
+    # when an item is found in the PLAYQ, first process all jobs in preQ!
+    while True:
+        # get first item in preQ and check if its a list, if so run it
+        preQ_job = PREQ.get()
+        if isinstance(preQ_job, list):
+            print('Found a pre-run queue item: %s', str(preQ_job))
+            # statements to process pre-Q job
+
+        # if it wasnt a list, but something is there, something is wrong
+        elif preQ_job is not None:
+            print('WARNING: non list item found in pre-run queue')
+            break
+        else:
+            print('INFO: processed all items in pre-run queue')
+            break #stop processing pre-Q if its empty
+
+    print('Processing %s', str(playbookjob))
+    # statements to process playbookjob.

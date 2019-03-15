1bf4b01eeef46fd98e48301101de238cad1a0315
Jeff Hibberd <jeff@jeffhibberd.com>
2019-03-15 18:09:34 +0000

    datestamp messages, keep only 20 in redis

--- anmad_queues.py
+++ anmad_queues.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Anmad queue module."""
+import datetime
 import pickle
 import redis
 from hotqueue import HotQueue
@@ -13,6 +14,11 @@ def read_queue(queue):
     for msg in redis_conn.lrange(queue.key, 0, -1):
         output.append(pickle.loads(msg))
     return output
+
+def trim_queue(queue, length):
+    """Trims a redis queue to last N items."""
+    redis_conn = redis.Redis()
+    redis_conn.ltrim(queue.key, -length, -1)
 
 
 class AnmadQueues:
@@ -30,20 +36,21 @@ class AnmadQueues:
         self.queue_list = []
         self.queue_list = read_queue(self.queue)
         self.info_list = []
+        trim_queue(self.info, 20)
         self.info_list = read_queue(self.info)
-
+        self.info_list.reverse()
 
     def prequeue_job(self, job):
         """Adds an item to the pre-run queue."""
         anmad_logging.LOGGER.info("Pre-Queuing %s", [job])
+        self.info.put([datetime.datetime.now(), "Pre-Queuing ", str([job])])
         self.prequeue.put([job])
-
 
     def queue_job(self, job):
         """Adds an item to the run queue."""
-        anmad_logging.LOGGER.info("Queuing %s", job)
+        anmad_logging.LOGGER.info("Queuing %s", str(job))
+        self.info.put([datetime.datetime.now(), "Queuing ", str(job)])
         self.queue.put(job)
-
 
     def clear(self):
         """Clears all job queues."""

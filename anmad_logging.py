7c9b4a8fb27b25b01c7f81677c278da96a0e0d2a
Jeff Hibberd <jeff@jeffhibberd.com>
2019-04-15 18:16:08 +0100

    fix indentation

--- anmad_logging.py
+++ anmad_logging.py
@@ -41,9 +41,9 @@ if anmad_args.ARGS.debug:
 
 # log main arguments used
 LOGGER.debug("config file: %s",
-            anmad_args.ARGS.configfile
-            if anmad_args.ARGS.configfile is not None
-            else anmad_args.DEFAULT_CONFIGFILE)
+             anmad_args.ARGS.configfile
+             if anmad_args.ARGS.configfile is not None
+             else anmad_args.DEFAULT_CONFIGFILE)
 LOGGER.debug("vault password file: %s", anmad_args.ARGS.vault_password_file)
 LOGGER.debug("ssh id: %s", anmad_args.ARGS.ssh_id)
 LOGGER.debug("venv: %s", anmad_args.ARGS.venv)
@@ -52,9 +52,9 @@ LOGGER.debug("inventorylist: %s", " ".join(anmad_args.ARGS.inventories))
 LOGGER.debug("maininventory: %s", anmad_args.MAININVENTORY)
 if anmad_args.ARGS.pre_run_playbooks:
     LOGGER.debug("pre_run_playbooks: %s",
-                " ".join(anmad_args.ARGS.pre_run_playbooks))
+                 " ".join(anmad_args.ARGS.pre_run_playbooks))
     LOGGER.debug("PRERUN_LIST: %s",
-                " ".join(anmad_args.PRERUN_LIST))
+                 " ".join(anmad_args.PRERUN_LIST))
 LOGGER.debug("playbooks: %s", " ".join(anmad_args.ARGS.playbooks))
 LOGGER.debug("RUN_LIST: %s", " ".join(anmad_args.RUN_LIST))
 LOGGER.debug("playbook_root_dir: %s", anmad_args.ARGS.playbook_root_dir)

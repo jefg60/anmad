4f05bd162e5c98b2e255a35cf1235b880994e2dd
Jeff Hibberd <jeff@jeffhibberd.com>
2019-06-03 15:01:27 +0100

    add unittest for version module

---
+++ test_version.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Tests for anmad_version module."""
+
+import unittest
+import anmad_version
+
+class TestVersion(unittest.TestCase):
+    """Tests for anmad_version module."""
+
+
+    def test_version(self):
+        """Tests for version constant."""
+        version = anmad_version.VERSION
+        self.assertEqual(version, "0.14.4")
+
+
+if __name__ == '__main__':
+    unittest.main()

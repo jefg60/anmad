2d3e3631659d4fe17ac53ef70825e58d9c3655ba
Jeff Hibberd <jeff@jeffhibberd.com>
2019-06-04 03:37:31 +0100

    update test_buttonfuncs

--- test_buttonfuncs.py
+++ test_buttonfuncs.py
@@ -1,32 +1,61 @@
 #!/usr/bin/env python3
 """Tests for anmad_buttons module."""
 
+import logging
+import os
 import unittest
+
+import __main__ as main
+
 import anmad_buttonfuncs
 
 class TestVersion(unittest.TestCase):
     """Tests for anmad_buttons module."""
 
-
-    def test_1arg_buttonlist(self):
-        """Test buttonlist with one list."""
-        buttons = anmad_buttonfuncs.buttonlist(['play1.yml', 'play2.yaml'])
-        self.assertIsNotNone(buttons)
-        self.assertEqual(len(buttons), 2)
-        self.assertEqual(buttons, ['play1.yml', 'play2.yaml'])
-
-    def test_2arg_buttonlist(self):
-        """Test buttonlist joins two lists."""
-        buttons = anmad_buttonfuncs.buttonlist(['play1.yml', 'play2.yaml'], ['prerun1.yaml'])
-        self.assertIsNotNone(buttons)
-        self.assertEqual(len(buttons), 3)
-        self.assertEqual(buttons, ['prerun1.yaml', 'play1.yml', 'play2.yaml'])
+    def setUp(self):
+        self.playbooks = ['deploy.yaml', 'deploy2.yaml']
+        self.pre_run_playbooks = ['deploy4.yaml']
+        self.playbookroot = 'samples'
+        self.logger = logging.getLogger(os.path.basename(main.__file__))
+        self.testextras = ['deploy3.yaml']
+        for x in range(5, 9):
+            dx = [('deploy' + str(x) + '.yaml')]
+            self.testextras.extend(dx)
+        self.testextras.extend(['deploy9.yml'])
 
     def test_noarg_buttonlist(self):
         """Test buttonlist behavior without args."""
         with self.assertRaises(TypeError):
-            self.buttons = anmad_buttonfuncs.buttonlist()
+            anmad_buttonfuncs.buttonlist()
 
+    def test_nopre_buttonlist(self):
+        """Test buttonlist without prerun."""
+        buttons = anmad_buttonfuncs.buttonlist(self.playbooks)
+        self.assertIsNotNone(buttons)
+        self.assertEqual(len(buttons), 2)
+        self.assertEqual(buttons, ['deploy.yaml', 'deploy2.yaml'])
+
+    def test_prerun_buttonlist(self):
+        """Test buttonlist with prerun."""
+        buttons = anmad_buttonfuncs.buttonlist(
+            self.playbooks, self.pre_run_playbooks)
+        self.assertIsNotNone(buttons)
+        self.assertEqual(len(buttons), 3)
+        self.assertEqual(buttons, ['deploy4.yaml', 'deploy.yaml', 'deploy2.yaml'])
+
+    def test_noarg_extraplays(self):
+        """Test extraplays behavior without args."""
+        with self.assertRaises(TypeError):
+            anmad_buttonfuncs.extraplays()
+
+    def test_nopre_extraplays(self):
+        """Test extraplays behavior without prerun.
+        adds pre_run_playbooks to expected list of extras for this test."""
+        self.testextras.extend(self.pre_run_playbooks)
+        self.testextras.sort()
+        extraplaybooks = anmad_buttonfuncs.extraplays(
+            self.logger, self.playbookroot, self.playbooks)
+        self.assertEqual(extraplaybooks, self.testextras)
 
 if __name__ == '__main__':
     unittest.main()

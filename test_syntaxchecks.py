8ad0682daf87bd030127dac4996be414bf2783ce
Jeff Hibberd <jeff@jeffhibberd.com>
2019-06-05 02:11:36 +0100

    adding tests

--- test_syntaxchecks.py
+++ test_syntaxchecks.py
@@ -17,27 +17,35 @@ class TestSyntaxCheck(unittest.TestCase):
         """Set up syntax check tests."""
         self.logger = logging.getLogger(os.path.basename(main.__file__))
         self.testplay = 'samples/deploy.yaml'
+        self.badplay = 'samples/deploy3.yaml'
         self.testinv = 'samples/inventory-internal'
         self.ansible_playbook_cmd = './venv/bin/ansible-playbook'
         self.vaultpw = './vaultpassword'
 
+    def run_syn_check_play_inv(self, play, vaultpw=None):
+        if vaultpw is not None:
+            output = anmad_syntaxchecks.syntax_check_play_inv(
+                self.logger,
+                play,
+                self.testinv,
+                self.ansible_playbook_cmd,
+                vaultpw)
+        else:
+            output = anmad_syntaxchecks.syntax_check_play_inv(
+                self.logger,
+                play,
+                self.testinv,
+                self.ansible_playbook_cmd)
+        return output
+
     def test_syntax_check_play_inv(self):
         """Tests for syntaxcheck_play_inv."""
-        output = anmad_syntaxchecks.syntax_check_play_inv(
-            self.logger,
-            self.testplay,
-            self.testinv,
-            self.ansible_playbook_cmd)
+        output = self.run_syn_check_play_inv(self.testplay)
         self.assertEqual(output, '')
 
     def test_syntax_check_play_inv_vaultpw(self):
         """Tests for syntaxcheck_play_inv."""
-        output = anmad_syntaxchecks.syntax_check_play_inv(
-            self.logger,
-            self.testplay,
-            self.testinv,
-            self.ansible_playbook_cmd,
-            self.vaultpw)
+        output = self.run_syn_check_play_inv(self.testplay, self.vaultpw)
         self.assertEqual(output, '')
 
 

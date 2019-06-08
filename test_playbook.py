7a41d4db9e8dfa267b56ee11a213442caa783d39
Jeff Hibberd <jeff@jeffhibberd.com>
2019-06-08 17:31:07 +0100

    pylint test_playbook

--- test_playbook.py
+++ test_playbook.py
@@ -22,43 +22,46 @@ class TestPlaybook(unittest.TestCase):
         self.testinv = 'samples/inventory-internal'
         self.ansible_playbook_cmd = './venv/bin/ansible-playbook'
         self.vaultpw = './vaultpassword'
+        self.playbookobject = anmad_playbook.ansibleRun(
+            self.logger,
+            self.testinv,
+            self.ansible_playbook_cmd)
 
     def test_ansible_playbook_novault_syn(self):
-        playbookobject = anmad_playbook.ansibleRun(
-            self.logger,
-            self.testinv,
-            self.ansible_playbook_cmd)
-        returned = playbookobject.run_playbook(self.testplay, syncheck=True)
-        self.assertEqual(returned.returncode , 0)
+        """Test run_playbook with no vault and syntax check."""
+        returned = self.playbookobject.run_playbook(
+            self.testplay, syncheck=True)
+        self.assertEqual(returned.returncode, 0)
 
     def test_ansible_playbook_novault_nosyn(self):
-        playbookobject = anmad_playbook.ansibleRun(
-            self.logger,
-            self.testinv,
-            self.ansible_playbook_cmd)
-        returned = playbookobject.run_playbook(self.testplay)
-        self.assertEqual(returned.returncode , 4)
-        returned = playbookobject.run_playbook(self.testplay, syncheck=False)
-        self.assertEqual(returned.returncode , 4)
+        """Test run_playbook with no vault and no syntax check."""
+        returned = self.playbookobject.run_playbook(self.testplay)
+        self.assertEqual(returned.returncode, 4)
+        returned = self.playbookobject.run_playbook(
+            self.testplay, syncheck=False)
+        self.assertEqual(returned.returncode, 4)
 
     def test_ansible_playbook_novault_nosyn_check(self):
-        playbookobject = anmad_playbook.ansibleRun(
-            self.logger,
-            self.testinv,
-            self.ansible_playbook_cmd)
-        returned = playbookobject.run_playbook(self.testplay, checkmode=True)
-        self.assertEqual(returned.returncode , 4)
+        """Test run_playbook with no vault,  no syntax check,
+        plus --check --diff mode."""
+        returned = self.playbookobject.run_playbook(
+            self.testplay, checkmode=True)
+        self.assertEqual(returned.returncode, 4)
 
     def test_ansible_playbook_vault_syn(self):
+        """Test run_playbook with vault."""
         playbookobject = anmad_playbook.ansibleRun(
             self.logger,
             self.testinv,
             self.ansible_playbook_cmd,
             self.vaultpw)
         returned = playbookobject.run_playbook(self.testplay, syncheck=True)
-        self.assertEqual(returned.returncode , 0)
+        self.assertEqual(returned.returncode, 0)
+
+    def test_syncheck_playbook(self):
+        """Test syncheck_playbook method."""
+        pass
 
 
 if __name__ == '__main__':
     unittest.main()
-

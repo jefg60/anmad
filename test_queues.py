4dcd996b3e9f8c8927db6eff415954225d18d86d
Jeff Hibberd <jeff@jeffhibberd.com>
2019-06-08 20:25:25 +0100

    pylint test_queues

--- test_queues.py
+++ test_queues.py
@@ -10,20 +10,20 @@ class TestQueues(unittest.TestCase):
 
     def setUp(self):
         """Setup some test queues."""
-        self.preq1 = 'prequeue_test1.yml'
-        self.preq2 = 'prequeue_test2.yml'
-        self.q1 = ['queue_test1.yml', 'queue_test2.yaml']
-        self.q2 = ['queue_test3.yml', 'queue_test4.yaml']
+        self.prequeue1 = 'prequeue_test1.yml'
+        self.prequeue2 = 'prequeue_test2.yml'
+        self.queue1 = ['queue_test1.yml', 'queue_test2.yaml']
+        self.queue2 = ['queue_test3.yml', 'queue_test4.yaml']
         self.queues = anmad_queues.AnmadQueues('test_prerun', 'test_playbooks', 'test_info')
         self.queues.clear()
         self.queues.clearinfo()
-        self.queues.prequeue_job(self.preq1)
-        self.queues.prequeue_job(self.preq2)
-        self.queues.queue_job(self.q1)
-        self.queues.queue_job(self.q2)
+        self.queues.prequeue_job(self.prequeue1)
+        self.queues.prequeue_job(self.prequeue2)
+        self.queues.queue_job(self.queue1)
+        self.queues.queue_job(self.queue2)
         # put 105 items in the info queue to fill it up
-        for x in range(0, 105):
-            self.queues.info.put(x)
+        for num in range(0, 105):
+            self.queues.info.put(num)
         self.queues.update_job_lists()
 
     def tearDown(self):
@@ -46,19 +46,20 @@ class TestQueues(unittest.TestCase):
     def test_getfromqueues(self):
         """Test that queues can be consumed in correct order."""
         # get first item from each queue
-        self.assertEqual(self.queues.prequeue.get(), [self.preq1])
-        self.assertEqual(self.queues.queue.get(), self.q1)
+        self.assertEqual(self.queues.prequeue.get(), [self.prequeue1])
+        self.assertEqual(self.queues.queue.get(), self.queue1)
         self.queues.update_job_lists()
         self.assertEqual(len(self.queues.queue_list), 1)
         self.assertEqual(len(self.queues.prequeue_list), 1)
         # get second item from each queue
-        self.assertEqual(self.queues.prequeue.get(), [self.preq2])
-        self.assertEqual(self.queues.queue.get(), self.q2)
+        self.assertEqual(self.queues.prequeue.get(), [self.prequeue2])
+        self.assertEqual(self.queues.queue.get(), self.queue2)
         self.queues.update_job_lists()
         self.assertEqual(len(self.queues.queue_list), 0)
         self.assertEqual(len(self.queues.prequeue_list), 0)
 
     def test_clear_queues(self):
+        """Test that clear_queues clears queues."""
         self.queues.clear()
         self.queues.clearinfo()
         self.queues.update_job_lists()

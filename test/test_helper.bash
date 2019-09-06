cbe5409e7854682af811a312e86cabcda9a39ef6
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-06 11:18:00 +0100

    correct version number in tests

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -8,5 +8,5 @@ teardown() {
 export printvault=/vagrant/print_vault_value.py
 export pylint="pylint"
 export program=/vagrant/anmad_buttons.py
-export version=0.16.0
+export version=0.16.1
 source ~/venv/bin/activate

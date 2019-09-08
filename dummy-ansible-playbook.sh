d97f6fe59d7613c7ebab6dc22df2a648f3ead3d2
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-08 11:54:47 +0100

    dummy-ansible-playbook test includes one with a path

--- dummy-ansible-playbook.sh
+++ dummy-ansible-playbook.sh
@@ -1,5 +1,5 @@
 perl -MPOSIX -e '$0="ansible-playbook"; pause' &
-perl -MPOSIX -e '$0="ansible-playbook"; pause' &
+perl -MPOSIX -e '$0="/opt/bin/ansible-playbook"; pause' &
 perl -MPOSIX -e '$0="ansible-playbook"; pause' &
 perl -MPOSIX -e '$0="ansible-playbook"; pause' &
 perl -MPOSIX -e '$0="ansible-playbook"; pause' &

580d2e70d175848deb722828785056e6eb2a09b4
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-09 21:14:49 +0100

    change dummy playbook script used in tests

--- dummy-ansible-playbook.sh
+++ dummy-ansible-playbook.sh
@@ -1,5 +1,2 @@
-perl -MPOSIX -e '$0="ansible-playbook"; pause' &
-perl -MPOSIX -e '$0="/opt/bin/ansible-playbook"; pause' &
-perl -MPOSIX -e '$0="ansible-playbook"; pause' &
-perl -MPOSIX -e '$0="ansible-playbook"; pause' &
-perl -MPOSIX -e '$0="ansible-playbook"; pause' &
+perl -MPOSIX -e '$0="/opt/bin/ansible-playbook param1 param2 /srv/config/deploy.yaml"; pause' &
+perl -MPOSIX -e '$0="/opt/bin/ansible-playbook /srv/config/deploy2.yaml"; pause' &

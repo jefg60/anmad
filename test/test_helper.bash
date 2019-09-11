49c248c103826826512d24c5bad082740b28f23c
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-11 13:55:36 +0100

    new version number

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -8,5 +8,5 @@ teardown() {
 export printvault=/vagrant/print_vault_value.py
 export pylint="pylint"
 export program=/vagrant/anmad_buttons.py
-export version=0.16.5
+export version=0.16.6
 source ~/venv/bin/activate

3d205e1604374912ff71c3191823b98118a11a45
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-11 17:58:34 +0100

    attempt to make process module playbook independent

--- anmad/process.py
+++ anmad/process.py
@@ -2,11 +2,11 @@
 import fnmatch
 import psutil
 
-def get_ansible_playbook_procs():
+def get_ansible_playbook_procs(cmdline_search='ansible-playbook'):
     """Get list of processes that match *ansible-playbook."""
     proclist = [p.info for p in
                 psutil.process_iter(attrs=['pid', 'name', 'cmdline'])
-                if fnmatch.filter(p.info['cmdline'], '*ansible-playbook*')]
+                if fnmatch.filter(p.info['cmdline'], '*' + cmdline_search + '*')]
     return proclist
 
 def kill(pid):
@@ -14,9 +14,10 @@ def kill(pid):
     process = psutil.Process(pid)
     process.kill()
 
-def killall():
+def killall(**kwargs):
     """Killall ansible-playbook."""
-    proclist = get_ansible_playbook_procs()
+    playtokill = kwargs.get('playtokill', 'ansible-playbook')
+    proclist = get_ansible_playbook_procs(cmdline_search=playtokill)
     pids = [li['pid'] for li in proclist]
     for pid in pids:
         kill(pid)

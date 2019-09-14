c56b062fab7c3018c9b0d79f0fa5b89383d9eae6
Jeff Hibberd <jeff@jeffhibberd.com>
2019-09-14 18:17:28 +0100

    more request tests

--- test_requests.py
+++ test_requests.py
@@ -11,12 +11,35 @@ class TestReturnCodes(unittest.TestCase):
     """Test http return codes."""
 
     def setUp(self):
-        pass
+        self.baseurl = "http://localhost:9999/"
 
     def test_ara_button(self):
         """Test the ara button."""
-        output = requests.get("http://localhost:9999/")
+        output = requests.get(self.baseurl)
         self.assertIn('/ara', output.text)
+
+    def test_clearall_button(self):
+        """Test the clearall button."""
+        output = requests.get(self.baseurl + 'clearqueues')
+        self.assertEqual(200, output.status_code)
+        self.assertIn('Clear redis queues requested.', output.text)
+
+    def test_runall_button(self):
+        """Test the runall button."""
+        requests.get(self.baseurl + 'clearqueues')
+        output = requests.get(self.baseurl + 'runall')
+        self.assertEqual(200, output.status_code)
+        self.assertIn("<h2>Queued Jobs:</h2>\n\t<h3>(multiples between [] will be concurrently run)</h3>\n\t\n\t\n\t<h3>[&#39;/vagrant/samples/deploy.yaml&#39;, &#39;/vagrant/samples/deploy2.yaml&#39;]</h3>", output.text)
+        self.assertIn(
+            "<h3>[&#39;/vagrant/samples/deploy4.yaml&#39;]</h3>", output.text)
+
+    def test_otherplays(self):
+        """Test the otherplays page."""
+        output = requests.get(self.baseurl + 'otherplays')
+        self.assertEqual(200, output.status_code)
+        self.assertIn("run deploy6.yaml", output.text)
+        self.assertIn("run deploy9.yml", output.text)
+        self.assertIn("Back to main interface", output.text)
 
 
 if __name__ == '__main__':

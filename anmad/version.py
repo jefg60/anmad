72cb97ede7754787ffbdb46f37fb6355e2caf358
Jeff Hibberd <jeff@jeffhibberd.com>
2019-10-12 18:03:40 +0100

    version 0.16.11

--- anmad/version.py
+++ anmad/version.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 """Module to set version number for print_vault_value and anmad modules"""
-VERSION = "0.16.10"
+VERSION = "0.16.11"
--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,5 +1,5 @@
 export printvault=/vagrant/print_vault_value.py
 export pylint=/home/vagrant/venv/bin/pylint
 export program=/vagrant/anmad_buttons.py
-export version=0.16.10
+export version=0.16.11
 export python=/home/vagrant/venv/bin/python

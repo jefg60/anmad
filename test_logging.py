e1b68e46154fa54b408b6dbb972e8205ee99b79e
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-10 13:10:58 +0100

    update test module to use new logsetup function param

--- test_logging.py
+++ test_logging.py
@@ -2,7 +2,9 @@
 """Tests for anmad.logging module."""
 
 import anmad.logging
+import anmad.args
 
 if __name__ == '__main__':
-    LOGGER = anmad.logging.logsetup()
+    ARGS = anmad.args.parse_args()
+    LOGGER = anmad.logging.logsetup(ARGS)
     LOGGER.debug("anmad.logging test ok")

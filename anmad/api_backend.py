3062d14816563d77bb5d7a9a14f58b0baeddc4e2
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-12 16:57:57 +0100

    split interface and api backends

--- anmad/api_backend.py
+++ anmad/api_backend.py
@@ -1,76 +1,10 @@
 #!/usr/bin/env python3
 """Functions for anmad_buttons."""
 
-import os
-import subprocess
 from flask import abort, redirect
 
 import anmad.yaml
-
-def basename(path):
-    """Simple func to be used as jinja2 filter."""
-    return os.path.basename(path)
-
-def buttonlist(playbooks, prerun=None):
-    """Get a list of allowed playbook buttons."""
-    try:
-        my_buttonlist = (prerun + playbooks)
-    except TypeError:
-        my_buttonlist = (playbooks)
-    return my_buttonlist
-
-def extraplays(prerun=None, **config):
-    """Get a list of yaml files in root dir that arent in buttonlist()."""
-    yamlfiles = anmad.yaml.find_yaml_files(
-        config["logger"], config["args"].playbook_root_dir)
-    yamlbasenames = []
-    for yml in yamlfiles:
-        yamlbasenames.append(os.path.basename(yml))
-    if prerun is None:
-        my_buttonlist = buttonlist(config["args"].playbooks)
-    else:
-        my_buttonlist = buttonlist(config["args"].playbooks,
-            config["args"].pre_run_playbooks)
-    extraplaybooks = list(set(yamlbasenames) - set(my_buttonlist))
-    extraplaybooks.sort()
-    return extraplaybooks
-
-#def oneplaybook(logger, queues, playbook, playbooklist, playbook_root_dir):
-def oneplaybook(playbook, playbooklist, **config):
-    """Queues one playbook, only if its in the playbooklist."""
-    if playbook not in playbooklist:
-        config["logger"].warning("API request for %s DENIED", str(playbook))
-        abort(404)
-    my_runlist = [config["args"].playbook_root_dir + '/' + playbook]
-    config["logger"].info("Queueing %s", str(my_runlist))
-    config["queues"].queue_job(my_runlist)
-
-def service_status(service):
-    """Check a service status."""
-    servicestatus = subprocess.run(
-        ['systemctl',
-         '--property',
-         'StateChangeTimestamp,ActiveState,SubState',
-         '--value',
-         'show',
-         service],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        check=False)
-
-    lines = servicestatus.stdout.splitlines()
-    active_state = lines[0]
-    sub_state = lines[1]
-    state_change_timestamp = lines[2]
-
-    if state_change_timestamp != '':
-        state_change_timestamp = str(' since ' + lines[2])
-    active_state = lines[0]
-    return {"service": service,
-            "active_state": active_state,
-            "sub_state": sub_state,
-            "state_change_timestamp": state_change_timestamp}
+import anmad.interface_backend
 
 def runall(**config):
     """Run all playbooks after verifying that files exist."""
@@ -94,11 +28,20 @@ def runall(**config):
     config["logger"].debug("Redirecting to control page")
     return redirect(config["baseurl"])
 
+def oneplaybook(playbook, playbooklist, **config):
+    """Queues one playbook, only if its in the playbooklist."""
+    if playbook not in playbooklist:
+        config["logger"].warning("API request for %s DENIED", str(playbook))
+        abort(404)
+    my_runlist = [config["args"].playbook_root_dir + '/' + playbook]
+    config["logger"].info("Queueing %s", str(my_runlist))
+    config["queues"].queue_job(my_runlist)
+
 def configuredplaybook(playbook, **config):
     """Runs one playbook, if its one of the configured ones."""
-    anmad.button_funcs.oneplaybook(
+    oneplaybook(
         playbook,
-        anmad.button_funcs.buttonlist(
+        anmad.interface_backend.buttonlist(
             config["args"].pre_run_playbooks,
             config["args"].playbooks),
         **config)
@@ -108,9 +51,9 @@ def configuredplaybook(playbook, **config):
 
 def otherplaybook(playbook, **config):
     """Runs one playbook, if its one of the other ones found by extraplays."""
-    anmad.button_funcs.oneplaybook(
+    oneplaybook(
         playbook,
-        anmad.button_funcs.extraplays(**config),
+        anmad.interface_backend.extraplays(**config),
         **config)
     config["logger"].debug("Redirecting to others page")
     config["queues"].update_job_lists()
---
+++ anmad/interface_backend.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Functions for anmad_buttons."""
+
+import os
+import subprocess
+
+import anmad.yaml
+
+def basename(path):
+    """Simple func to be used as jinja2 filter."""
+    return os.path.basename(path)
+
+def buttonlist(playbooks, prerun=None):
+    """Get a list of allowed playbook buttons."""
+    try:
+        my_buttonlist = (prerun + playbooks)
+    except TypeError:
+        my_buttonlist = (playbooks)
+    return my_buttonlist
+
+def extraplays(prerun=None, **config):
+    """Get a list of yaml files in root dir that arent in buttonlist()."""
+    yamlfiles = anmad.yaml.find_yaml_files(
+        config["logger"], config["args"].playbook_root_dir)
+    yamlbasenames = []
+    for yml in yamlfiles:
+        yamlbasenames.append(os.path.basename(yml))
+    if prerun is None:
+        my_buttonlist = buttonlist(config["args"].playbooks)
+    else:
+        my_buttonlist = buttonlist(config["args"].playbooks,
+            config["args"].pre_run_playbooks)
+    extraplaybooks = list(set(yamlbasenames) - set(my_buttonlist))
+    extraplaybooks.sort()
+    return extraplaybooks
+
+def service_status(service):
+    """Check a service status."""
+    servicestatus = subprocess.run(
+        ['systemctl',
+         '--property',
+         'StateChangeTimestamp,ActiveState,SubState',
+         '--value',
+         'show',
+         service],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=False)
+
+    lines = servicestatus.stdout.splitlines()
+    active_state = lines[0]
+    sub_state = lines[1]
+    state_change_timestamp = lines[2]
+
+    if state_change_timestamp != '':
+        state_change_timestamp = str(' since ' + lines[2])
+    active_state = lines[0]
+    return {"service": service,
+            "active_state": active_state,
+            "sub_state": sub_state,
+            "state_change_timestamp": state_change_timestamp}

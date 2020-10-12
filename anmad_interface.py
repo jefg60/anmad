eab448c2a32b73bc737866cd1b01b00816cf2f8d
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-12 17:12:01 +0100

    correct module import name

--- anmad_interface.py
+++ anmad_interface.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Run dev test server."""
-import anmad.buttons
-application = anmad.buttons.flaskapp
+import anmad.routes
+application = anmad.routes.flaskapp
 
 if __name__ == "__main__":
-    if not anmad.buttons.config["args"].dryrun:
+    if not anmad.routes.config["args"].dryrun:
         application.run(host='0.0.0.0', port=9999, debug=True)

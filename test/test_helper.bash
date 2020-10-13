d30f50c7843e4180459526d96acd261cedc3b96c
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-13 11:59:00 +0100

    update version number for tests

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,5 +1,5 @@
 export printvault=/vagrant/print_vault_value.py
 export pylint=/home/vagrant/venv/bin/pylint
 export program=/vagrant/anmad_interface.py
-export version=0.17.2
+export version=0.18.0
 export python=/home/vagrant/venv/bin/python

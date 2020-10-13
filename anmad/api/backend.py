57c5516d55b0c8714c1cdf6f74605a0635544c33
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-13 10:07:31 +0100

    rename files for new structure

--- anmad/api/backend.py
+++ anmad/api/backend.py
@@ -3,12 +3,13 @@
 
 from flask import abort, redirect
 
-import anmad.yaml
-import anmad.interface_backend
+from anmad.yaml import list_missing_files
+from anmad.daemon.process import get_ansible_playbook_procs, kill
+import interface.backend
 
 def runall(**config):
     """Run all playbooks after verifying that files exist."""
-    problemfile = anmad.yaml.list_missing_files(
+    problemfile = list_missing_files(
         config["logger"],
         config["args"].prerun_list)
     if problemfile:
@@ -41,7 +42,7 @@ def configuredplaybook(playbook, **config):
     """Runs one playbook, if its one of the configured ones."""
     oneplaybook(
         playbook,
-        anmad.interface_backend.buttonlist(
+        interface.backend.buttonlist(
             config["args"].pre_run_playbooks,
             config["args"].playbooks),
         **config)
@@ -53,19 +54,19 @@ def otherplaybook(playbook, **config):
     """Runs one playbook, if its one of the other ones found by extraplays."""
     oneplaybook(
         playbook,
-        anmad.interface_backend.extraplays(**config),
+        interface.backend.extraplays(**config),
         **config)
     config["logger"].debug("Redirecting to others page")
     config["queues"].update_job_lists()
     return redirect(config["baseurl"] + 'otherplays')
 
-def kill(requestedpid, **config):
+def kill_proc_by_pid(requestedpid, **config):
     """Kill a proc by PID.
     Hopefully a PID thats verified by psutil to be an ansible-playbook!"""
-    proclist = anmad.process.get_ansible_playbook_procs()
+    proclist = get_ansible_playbook_procs()
     pids = [li['pid'] for li in proclist]
     if requestedpid in pids:
-        anmad.process.kill(requestedpid)
+        kill(requestedpid)
         config["logger"].warning("KILLED pid %s on request", requestedpid)
         for proc in proclist:
             if proc['pid'] == requestedpid:

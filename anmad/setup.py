b134d36930507afef343f017b07d52bb5317202b
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-14 14:32:32 +0100

    fixing setup.py

--- anmad/setup.py
+++
@@ -1,4 +0,0 @@
-"""anmad module setup.py."""
-from setuptools import setup, find_packages
-
-setup(name='anmad', version='0.18.0', packages=find_packages())
---
+++ setup.py
@@ -0,0 +1,10 @@
+"""anmad module setup.py."""
+from setuptools import setup, find_packages
+
+setup(name='anmad',
+    version='0.18.0',
+    package_dir={
+        '': 'anmad',
+    },
+    packages=find_packages()
+    )

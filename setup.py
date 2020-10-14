78bef092b3cc6a38ab53faa63b3560f98d42d4d7
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-14 17:32:57 +0100

    setup.py metadata, requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,22 @@ from setuptools import setup, find_packages
 
 setup(name='anmad',
     version='0.18.2',
+    description='Creates a simple api and a browser interface for running ansible playbooks.',
+    download_url='https://github.com/jefg60/anmad',
+    author='Jeff Hibberd',
+    author_email='jeff@jeffhibberd.com',
+    license='GPLv3',
+    install_requires=[
+        'ansible',
+        'ansible-vault >=1.2.1',
+        'configargparse',
+        'flask',
+        'hotqueue',
+        'psutil',
+        'pyyaml',
+        'redis',
+        'requests',
+        'ssh_agent_setup',
+        ],
     packages=find_packages()
     )

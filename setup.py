3a953102a7e69b479eae4a23514eede2779ebf0d
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-21 14:25:13 +0100

    v0.18.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='anmad',
-    version='0.18.2',
+    version='0.18.3',
     description='Creates a simple api and a browser interface for running ansible playbooks.',
     url='https://github.com/jefg60/anmad',
     author='Jeff Hibberd',

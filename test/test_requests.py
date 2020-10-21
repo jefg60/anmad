fcca1a1e79b2af9b82b340c01c105323894109ce
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-21 14:11:49 +0100

    remove ara button test

--- test/test_requests.py
+++ test/test_requests.py
@@ -21,11 +21,6 @@ class TestReturnCodes(unittest.TestCase):
         self.assertIsNotNone(datestring)
         self.assertTrue(datetime.datetime.strptime(datestring.group(0), self.dateformat))
 
-    def test_ara_button_exists(self):
-        """Test the ara button."""
-        output = requests.get(self.baseurl)
-        self.assertIn('/ara', output.text)
-
     def test_clearall_button(self):
         """Test the clearall button."""
         output = requests.get(self.baseurl + 'clearqueues')

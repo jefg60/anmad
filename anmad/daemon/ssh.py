6f229655b3949e3a378a02a4dd62f45192e24cce
Jeff Hibberd <jeff@jeffhibberd.com>
2020-11-12 11:12:26 +0000

    handle incorrect ssh key phrase

--- anmad/daemon/ssh.py
+++ anmad/daemon/ssh.py
@@ -19,10 +19,14 @@ def add_ssh_key_to_agent(logger, key_file, ssh_askpass=None):
                        env=my_env,
                        check=True,
                        stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL
+                       stderr=subprocess.DEVNULL,
+                       timeout=5
                        )
     except subprocess.CalledProcessError as failed_to_add_sshkey:
         logger.exception("Exception adding ssh key, shutting down")
         raise Exception from failed_to_add_sshkey
+    except subprocess.TimeoutExpired as ssh_key_timeout:
+        logger.exception("Timeout adding ssh key, check passphrase?")
+        raise Exception from ssh_key_timeout
     else:
         logger.info("SSH key loaded")

e32db5eec79ffe38cf2bb9b4f0182aa8716dfb1b
Jeff Hibberd <jeff@jeffhibberd.com>
2020-11-17 13:11:26 +0000

    fix timeout on unit test

--- test/test_run.py
+++ test/test_run.py
@@ -23,7 +23,7 @@ class TestPlaybook(unittest.TestCase):
         self.testinv = '/vagrant/samples/inventory-internal'
         self.ansible_playbook_cmd = '/home/vagrant/venv/bin/ansible-playbook'
         self.vaultpw = '/vagrant/test/vaultpassword'
-        self.timeout = 3
+        self.timeout = 5
         self.playbookobject = AnmadRun(
             self.logger,
             self.testinv,

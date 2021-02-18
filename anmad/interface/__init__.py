0c4092edf6c1b1021f01050022aa424212f6db7a
Jeff Hibberd <jeff@jeffhibberd.com>
2021-02-18 15:19:11 +0000

    linting gunicorn changes

--- anmad/interface/__init__.py
+++ anmad/interface/__init__.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """flaskapp as application object."""
+import sys
+import anmad.interface.routes as anmadroutes
 
 def main():
-    import anmad.interface.routes as anmadroutes
+    """Normal entry point."""
     application = anmadroutes.flaskapp
     return application
 
 # Gunicorn entry point generator
-def app(*args, **kwargs):
+def app(**kwargs):
+    """Gunicorn entry point."""
     # Gunicorn CLI args are useless.
     # https://stackoverflow.com/questions/8495367/
     #
     # Start the application in modified environment.
     # https://stackoverflow.com/questions/18668947/
     #
-    import sys
     sys.argv = ['--gunicorn']
     for k in kwargs:
         sys.argv.append("--" + k)

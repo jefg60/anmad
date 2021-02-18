64c4137e3758f6f4e5a2e07f08e08fb152fc56f9
Jeff Hibberd <jeff@jeffhibberd.com>
2021-02-18 15:01:13 +0000

    allow passing args to gunicorn

--- anmad/interface/__init__.py
+++ anmad/interface/__init__.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 """flaskapp as application object."""
-import anmad.interface.routes as anmadroutes
-application = anmadroutes.flaskapp
+
+def main():
+    import anmad.interface.routes as anmadroutes
+    application = anmadroutes.flaskapp
+    return application
+
+# Gunicorn entry point generator
+def app(*args, **kwargs):
+    # Gunicorn CLI args are useless.
+    # https://stackoverflow.com/questions/8495367/
+    #
+    # Start the application in modified environment.
+    # https://stackoverflow.com/questions/18668947/
+    #
+    import sys
+    sys.argv = ['--gunicorn']
+    for k in kwargs:
+        sys.argv.append("--" + k)
+        sys.argv.append(kwargs[k])
+    return main()

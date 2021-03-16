fb2663d0d11a673083ede357cf5e98aaf34e1053
Jeff Hibberd <jeff@jeffhibberd.com>
2021-03-16 13:11:52 +0000

    interface doesnt need a dryrun arg

--- anmad/interface/__main__.py
+++ anmad/interface/__main__.py
@@ -4,5 +4,4 @@ import anmad.interface.routes as anmadroutes
 application = anmadroutes.flaskapp
 
 if __name__ == "__main__":
-    if not anmadroutes.config["args"].dryrun:
-        application.run(host='0.0.0.0', port=9999, debug=True)
+    application.run(host='0.0.0.0', port=9999, debug=True)
